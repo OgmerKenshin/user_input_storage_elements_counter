4e1d778b8b80076495c2b95179eb86eff4d3dac8
unknown <kenshinogmer>
2024-10-30 11:41:08 +0800

    I set 5 arrays to store the user inputs

--- user_input_counter.py
+++ user_input_counter.py
@@ -3,3 +3,9 @@
 #insert a while loop so the input can be repeated
 #insert if and elif statements to organize which use input numbers go to which arrays
 #else statement's output is how many numbers have been input unto each of the 5 boxes
+
+one_to_10 = []
+elevn_to_20 = []
+twntyone_to_30 = []
+thrtyone_to_40 = []
+frtyone_to_50 = []
