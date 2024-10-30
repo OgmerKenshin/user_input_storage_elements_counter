66dbddf70be2d56307750cc199385fe36e31795b
unknown <kenshinogmer>
2024-10-30 14:47:00 +0800

    I added a try and except statement for errors

--- user_input_counter.py
+++ user_input_counter.py
@@ -9,37 +9,44 @@ elevn_to_20 = []
 twntyone_to_30 = []
 thrtyone_to_40 = []
 frtyone_to_50 = []
-
-num = int(input("enter a number here: "))
+try:
+    num = int(input("enter a number here: "))
+except:
+    print("pls input again")
 
 while True:
-    if num > 0 and num < 11:
-        print("your input has been placed in the 1-10 box: ")
-        one_to_10.append(num)
-        num = int(input("input another number:"))
-    elif num > 10 and num < 21:
-        print("your input has been stored in box 11-20")
-        elevn_to_20.append(num)
-        num = int(input("input another number:"))
-    elif num > 20 and num < 31:
-        print("your input has been stored in the 21-30 box")
-        twntyone_to_30.append(num)
-        num = int(input("input another number:"))
-    elif num > 30 and num < 41:
-        print("ur input is stored in box 31-40")
-        thrtyone_to_40.append(num)
-        num = int(input("input another number:"))
-    elif num > 40 and num < 51:
-        print("ur input has been placed in box 41-50")
-        frtyone_to_50.append(num)
-        num = int(input("input another number:"))
-    else:
-        print("invalid input, 1-10 = ",len(one_to_10))
-        print("11-20 = ",len(elevn_to_20))
-        print("21-30 = ",len(twntyone_to_30))
-        print("31-40 = ",len(thrtyone_to_40))
-        print("41-50 = ",len(frtyone_to_50))
-        break
+    try:
+         if num > 0 and num < 11:
+             print("your input has been placed in the 1-10 box: ")
+             one_to_10.append(num)
+             num = int(input("input another number:"))
+         elif num > 10 and num < 21:
+             print("your input has been stored in box 11-20")
+             elevn_to_20.append(num)
+             num = int(input("input another number:"))
+         elif num > 20 and num < 31:
+             print("your input has been stored in the 21-30 box")
+             twntyone_to_30.append(num)
+             num = int(input("input another number:"))
+         elif num > 30 and num < 41:
+              print("ur input is stored in box 31-40")
+              thrtyone_to_40.append(num)
+              num = int(input("input another number:"))
+         elif num > 40 and num < 51:
+             print("ur input has been placed in box 41-50")
+             frtyone_to_50.append(num)
+             num = int(input("input another number:"))
+         else:
+             print("invalid input")
+             print("1-10 = ",len(one_to_10))
+             print("11-20 = ",len(elevn_to_20))
+             print("21-30 = ",len(twntyone_to_30))
+             print("31-40 = ",len(thrtyone_to_40))
+             print("41-50 = ",len(frtyone_to_50))
+             break
+    except:
+         print("pls input again")
+         break
     
 
             
