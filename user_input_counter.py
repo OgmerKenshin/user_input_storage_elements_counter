8778e473363bc72a8bb34756db0c43b285cb65af
unknown <kenshinogmer>
2024-10-30 11:11:38 +0800

    pseudo code

---
+++ user_input_counter.py
@@ -0,0 +1,5 @@
+#set 5 boxes or arrays labeled 1-10, 11-20,etc...
+#num = int(input())
+#insert a while loop so the input can be repeated
+#insert if and elif statements to organize which use input numbers go to which arrays
+#else statement's output is how many numbers have been input unto each of the 5 boxes
